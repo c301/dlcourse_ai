8cd711857872b9cc847905d3c1b2384cf0ca4434
Anton Sivolapov <anton.sivolapov@gmail.com>
2019-03-14 14:24:54 -0700

    gradient through derivative

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -30,8 +30,27 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
+        # print("it", it)
+        # print(ix)
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
+        # print(x[ix])
+
+        plus_delta = x.copy()
+        minus_delta = x.copy()
+
+        plus_delta[ix] += delta
+        minus_delta[ix] -= delta
+
+        f_delta_plus = f( plus_delta )[0]
+        # print(f_delta_plus)
+        f_delta_minus = f( minus_delta )[0]
+        # print(f_delta_minus)
+
+        derivative = ( f_delta_plus - f_delta_minus ) / ( 2 * delta )
+        # print(derivative)
+        # print(derivative/( delta * delta ) )
+        # numeric_grad_at_ix = ( f_delta_plus - f_delta_minus ) / delta * derivative
+        numeric_grad_at_ix = derivative
 
         # TODO compute value of numeric gradient of f to idx
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
